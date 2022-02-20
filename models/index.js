0c82c0819ab93b16a755ea2bf46c73eeeea72581
jackarms <90922918+jackarms@users.noreply.github.com>
2022-02-20 05:24:20 -0600

    addition of post routes

--- models/index.js
+++ models/index.js
@@ -3,13 +3,27 @@ const Post = require("./Post");
 const User = require("./User");
 
 Post.belongsTo(User, {
-  foreignKey: "id",
+  foreignKey: "user_id",
 });
 
 Comment.belongsTo(User, {
-  foreignKey: "id",
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
 });
 
 Post.hasMany(Comment, {
-  foreignKey: "id",
+  foreignKey: "post_id",
 });
+
+module.exports = { User, Post, Comment };
