27d715b9ca780285e00f690a79ed7860b60ebe44
jackarms <90922918+jackarms@users.noreply.github.com>
2022-02-20 10:18:59 -0600

    creation of signup and login pages

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,43 @@
 const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
-  res.render("homepage");
+  Post.findAll({
+    attributes: ["id", "title", "post_text", "created_at"],
+    order: [["created_at", "DESC"]],
+    include: [
+      // {
+      //   model: Comment,
+      //   attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+      //   include: {
+      //     model: User,
+      //     attributes: ["username"],
+      //   },
+      // },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+      res.render("homepage", { posts });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -3,27 +3,28 @@ const Post = require("./Post");
 const User = require("./User");
 
 Post.belongsTo(User, {
-  foreignKey: "id",
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 Comment.belongsTo(User, {
-  foreignKey: "id",
+  foreignKey: "user_id",
 });
 
 Comment.belongsTo(Post, {
-  foreignKey: "id",
+  foreignKey: "post_id",
 });
 
 User.hasMany(Comment, {
-  foreignKey: "id",
+  foreignKey: "comment_id",
 });
 
 User.hasMany(Post, {
-  foreignKey: "id",
+  foreignKey: "user_id",
 });
 
 Post.hasMany(Comment, {
-  foreignKey: "id",
+  foreignKey: "comment_id",
 });
 
 module.exports = { User, Post, Comment };
