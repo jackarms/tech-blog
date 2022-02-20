36db5cd2ac3c88a3b2d1f49401385bfa6ddb2842
jackarms <90922918+jackarms@users.noreply.github.com>
2022-02-20 10:34:34 -0600

    creation of login and logout capabilities

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
+  console.log(req.session);
   Post.findAll({
     attributes: ["id", "title", "post_text", "created_at"],
     order: [["created_at", "DESC"]],
@@ -33,9 +34,13 @@ router.get("/", (req, res) => {
 });
 
 router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
   res.render("login");
 });
-
 router.get("/signup", (req, res) => {
   res.render("signup");
 });
